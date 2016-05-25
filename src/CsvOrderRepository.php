a38b3cb73f3c03067e159aaa024ceee614b0aa1e
Ken Lin <kenlin@kkbox.com>
2016-05-25 18:25:12 +0800

    Implement logOrder method to make example more real

--- src/CsvOrderRepository.php
+++ src/CsvOrderRepository.php
@@ -1,8 +1,21 @@
 <?php namespace Twk;
 
 class CsvOrderRepository implements OrderRepositoryInterface {
+
+    private $logFile;
+
+    public function __construct($logFile)
+    {
+        $this->logFile = __DIR__ . "/storage/$logFile";
+    }
+
     public function logOrder(Order $order)
     {
-        var_dump('log order in csv file');
+        $fp = fopen($this->logFile, 'w');
+        fputcsv($fp, [
+            'id' => $order->id,
+            'amount' => $order->amount,
+        ]);
+        fclose($fp);
     }
 }
